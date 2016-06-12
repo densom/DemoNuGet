edb969d18eea2b7b1c6826e1362f59a30ab564d9
Dennis Somerville <dennis.somerville@gmail.com>
2016-06-12 07:45:48 -0400

    Initial commit.

---
+++ DemoNuGet.Tests/HelloWorldTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DemoNuGet.Tests
+{
+    [TestFixture]
+    public class HelloWorldTests
+    {
+        [Test]
+        public void SayHello_ReturnsProperString()
+        {
+            var result = HelloWorld.SayHello("Dennis");
+            Assert.That(result, Is.EqualTo("Hello Dennis"));
+        }
+    }
+}
